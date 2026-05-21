3bebc560649bdeb3f423f868baf6bd4879c521d0
Zach Waring <158355453+Byeafly@users.noreply.github.com>
2026-05-20 19:10:38 -0600

    tiny updates

--- front_end_Stack/solution_stack_shared/models/Order.cs
+++ front_end_Stack/solution_stack_shared/models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,20 @@ namespace solution_stack_shared.models
     public class Order
     {
         public string ID { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Adress { get; set; }
         public OrderStatus Status { get; set; }
     }
 
     public enum OrderStatus
     {
-        New,
-        InProgress,
-        Completed,
-        Cancelled
+        New = 0,
+        InProgress = 1,
+        Completed = 2,
+        Cancelled = 3
     }
 }
--- front_end_Stack/solution_stack_shared/models/OrderCreateDto.cs
+++ front_end_Stack/solution_stack_shared/models/OrderCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace solution_stack_shared.models
 {
     public class OrderCreateDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Adress { get; set; }
     }
 }
