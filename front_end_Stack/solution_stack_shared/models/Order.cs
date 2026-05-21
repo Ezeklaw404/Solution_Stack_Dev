28c2cd859521f03d915aa41f32a1ba1096bb9370
Zach Waring <158355453+Byeafly@users.noreply.github.com>
2026-05-20 18:15:55 -0600

    sdvsdc

---
+++ front_end_Stack/solution_stack_shared/models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution_stack_shared.models
+{
+    public class Order
+    {
+        public string ID { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        New,
+        InProgress,
+        Completed,
+        Cancelled
+    }
+}
